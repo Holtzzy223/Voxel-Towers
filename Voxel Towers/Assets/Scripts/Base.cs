d21e6a7dde4a284aa02168557730a75c859670bf
Holtzzy223 <70851222+Holtzzy223@users.noreply.github.com>
2021-04-07 20:42:00 -0400

    currency updates

--- Voxel Towers/Assets/Scripts/Base.cs
+++ Voxel Towers/Assets/Scripts/Base.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Base : MonoBehaviour
 {
     [SerializeField] int maxHP = 200;
     [SerializeField] int currentHP;
+    
     int CurrentHP { get { return currentHP; } }
-   
-
+    private ParticleSystem healthParticles;
     // Start is called before the first frame update
     void Awake()
     {
         
         currentHP = maxHP;
+        healthParticles = GetComponent<ParticleSystem>();
+        
+    }
+
+    private void Update()
+    {
+        if (currentHP <= maxHP * 0.5)
+        {
+            ParticleSystem.MainModule main = healthParticles.main;   
+            main.startColor = Color.yellow;
+        }
     }
 
     public void RemoveHP(int amount)
@@ -28,6 +40,8 @@ public class Base : MonoBehaviour
              * Pop Option to restart Level Quit to main?
              */
             Destroy(gameObject);
+            //Reload Scene Temp
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
     public void AddHP(int amount)
--- Voxel Towers/Assets/Scripts/PlayerBank.cs
+++ Voxel Towers/Assets/Scripts/PlayerBank.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerBank : MonoBehaviour
 {
     [SerializeField] int startBalance = 150;
     [SerializeField] int currentBalance;        //change once UI in place
     public int CurrentBalance {get  { return currentBalance;} }
-
+    public TextMeshProUGUI currencyText;
     private void Awake()
     {
         currentBalance = startBalance;
+        currencyText.text = "Currency: " + currentBalance;
+    }
+    private void Update()
+    {
+     //   currencyText.text = "Currency: " + currentBalance;
     }
 
-   public void Deposit(int amount) 
+    public void Deposit(int amount) 
     {
         currentBalance += Mathf.Abs(amount);
+        currencyText.text = "Currency: " + currentBalance;
     }
 
     public void Withdrawl(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        currencyText.text = "Currency: " + currentBalance;
     }
 }
