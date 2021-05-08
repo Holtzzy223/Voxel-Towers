f76f25a6dfea1880b854a65653b3d5ddd5f4c027
Holtzzy223 <70851222+Holtzzy223@users.noreply.github.com>
2021-05-08 11:57:07 -0400

    Buttons/Highlights
    + Tower choice UI
    + Grid highlight on hover

---
+++ Voxel Towers/Assets/Scripts/TowerUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerUI : MonoBehaviour
+{
+    public int ButtonChoice { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonChoice = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CursorChange()
+    {
+        Cursor.visible = false;
+    }
+}
--- Voxel Towers/Assets/Scripts/Waypoint.cs
+++ Voxel Towers/Assets/Scripts/Waypoint.cs
@@ -10,10 +10,15 @@ public class Waypoint : MonoBehaviour
     public GameObject grassMesh;
     public GameObject pathMesh;
     public GameObject path90Mesh;
-    
+    public TowerUI towerUI;
+    public Mesh mesh;
+    public Material material;
+
+
     public bool IsPlaceable { get   { return isPlaceable;}}
     private void Start()
     {
+     towerUI = FindObjectOfType<TowerUI>(); 
      ///  GameObject parentObject = GetComponentInParent<GameObject>();
      ///  if (parentObject != null)
      ///  {
@@ -25,11 +30,25 @@ public class Waypoint : MonoBehaviour
     }
     private void OnMouseOver()
     {
-
-        if (isPlaceable && Input.GetMouseButtonDown(0))
+        DrawHighLight();
+        PlaceTower();
+        
+        
+    }
+    public void PlaceTower()
+    {
+        if (isPlaceable && Input.GetMouseButtonDown(0) && towerUI.ButtonChoice != -1)
         {
-            bool isPlaced = towers[1].CreateTower(towers[1],transform.position);
+            bool isPlaced = towers[towerUI.ButtonChoice].CreateTower(towers[towerUI.ButtonChoice],transform.position);
             isPlaceable = !isPlaced;
         }
+        Cursor.visible = true;
+    }
+    void DrawHighLight()
+    {
+        Vector3 scale =new Vector3( 1f,1.1f,1f);
+        //Vector3 rangeIndicatorVector = new Vector3(range * rangeIndicatorMod, range * (rangeIndicatorMod * zDrawOffset), range * rangeIndicatorMod);
+        Matrix4x4 trsMatrix = Matrix4x4.TRS(transform.position, Quaternion.identity, scale);
+        Graphics.DrawMesh(mesh, trsMatrix, material, 1);
     }
 }
