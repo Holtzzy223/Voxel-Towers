b4fa669446584263b624e233b9131fd7d3234604
Holtzzy223 <70851222+Holtzzy223@users.noreply.github.com>
2021-05-21 11:19:29 -0400

    BFS first pass
    + Node Class
    + Grid Manager

---
+++ Voxel Towers/Assets/Scripts/GridManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public Vector2Int gridSize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    private void Awake()
+    {
+        CreateGrid();
+    }
+    public Node GetNode(Vector2Int coords)
+    {
+        if (grid.ContainsKey(coords))
+        {
+            return grid[coords];
+        }
+        return null;
+    }
+
+    private void CreateGrid()
+    {
+        for (int x = 0; x < gridSize.x;x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int coords = new Vector2Int(x,y);
+                grid.Add(coords, new Node(coords, true));
+                Debug.Log(grid[coords].coords + "=" + grid[coords].isTraversable);
+            }
+        }
+    }
+}
---
+++ Voxel Towers/Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node 
+{
+    public Vector2Int coords;
+    public bool isTraversable;
+    public bool isExplored;
+    public bool isPath;
+    public Node connectedNode;
+
+    public Node(Vector2Int coords, bool isTraversable) 
+    {
+        this.coords = coords;
+        this.isTraversable = isTraversable;
+    }
+}
